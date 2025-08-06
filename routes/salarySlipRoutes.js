b1729e079d4986a883ebb46f15da65f76dcb0874
Sakshi Gupta <sakshigupta.302004@gmail.com>
2025-08-06 11:00:04 +0530

    Corrected Routes

--- routes/salarySlipRoutes.js
+++ routes/salarySlipRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const salarySummaryController = require('../controllers/salarySummaryController');
 
-router.post('/salarysummary', salarySummaryController.saveSalarySummary);
-router.get('/salarysummary', salarySummaryController.getSalarySummary);
+router.post('/', salarySummaryController.saveSalarySummary);
+router.get('/', salarySummaryController.getSalarySummary);
 
 module.exports = router;
